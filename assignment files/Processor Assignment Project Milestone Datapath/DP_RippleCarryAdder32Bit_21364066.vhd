0ef5e82244f68533b447971ddf9b79fdf74fd3af
Faith Olopade <olopadef@tcd.ie>
2022-11-03 23:16:21 +0000

    fixed DP_RippleCarryAdder32Bit_21364066

--- assignment files/Processor Assignment Project Milestone Datapath/DP_RippleCarryAdder32Bit_21364066.vhd
+++ assignment files/Processor Assignment Project Milestone Datapath/DP_RippleCarryAdder32Bit_21364066.vhd
@@ -54,9 +54,7 @@ component DP_FullAdder_21364066
         );
     end component;
     
-signal C0_to_C1, C1_to_C2, C2_to_C3, C3_to_C4, C4_to_C5, C5_to_C6, C6_to_C7, C7_to_C8, C8_to_C9, C9_to_C10, C10_to_C11,  C11_to_C12, C12_to_13, C13_to_C14, 
-C14_to_C15, C15_to_C16, C16_to_C17, C17_to_C18, C18_to_C19, C19_to_C20, C20_to_C20, C21_to_C22, C22_to_C23, C23_to_C24, C24_to_C25, C25_to_C26, C26_to_C27, C27_to_C28, C28_to_C29,
-C29_to_C30, C30_to_C31, C31_to_COut : std_logic;    
+signal C0_to_C1, C1_to_C2, C2_to_C3, C3_to_C4, C4_to_C5, C5_to_C6, C6_to_C7, C7_to_C8, C8_to_C9, C9_to_C10, C10_to_C11,  C11_to_C12, C12_to_C13, C13_to_C14, C14_to_C15, C15_to_C16, C16_to_C17, C17_to_C18, C18_to_C19, C19_to_C20, C20_to_C21, C21_to_C22, C22_to_C23, C23_to_C24, C24_to_C25, C25_to_C26, C26_to_C27, C27_to_C28, C28_to_C29, C29_to_C30, C30_to_C31, C31_to_COut : std_logic;    
 
 begin
 
@@ -84,7 +82,7 @@ begin
      B => B(2),
      C_IN => C1_to_C2,
      SUM => SUM(2),
-     C_OUT => C2_to_C3, 
+     C_OUT => C2_to_C3
     );
 
   -- Instantiate Full Adder Bit 3
@@ -93,7 +91,7 @@ begin
     B => B(3),
     C_IN => C2_to_C3,
     SUM => SUM(3),
-    C_OUT => C3_to_C4,
+    C_OUT => C3_to_C4
   );
 
   -- Instantiate Full Adder Bit 4
@@ -102,7 +100,7 @@ begin
     B => B(4),
     C_IN => C3_to_C4,
     SUM => SUM(4),
-    C_OUT => C4_to_C5,
+    C_OUT => C4_to_C5
   );
 
   -- Instantiate Full Adder Bit 5
@@ -111,7 +109,7 @@ begin
     B => B(5),
     C_IN => C4_to_C5,
     SUM => SUM(5),
-    C_OUT => C5_to_C6,
+    C_OUT => C5_to_C6
   );
 
   -- Instantiate Full Adder Bit 6
@@ -120,7 +118,7 @@ begin
     B => B(6),
     C_IN => C5_to_C6,
     SUM => SUM(6),
-    C_OUT => C6_to_C7, 
+    C_OUT => C6_to_C7
   );
 
   -- Instantiate Full Adder Bit 7
@@ -129,7 +127,7 @@ begin
     B => B(7),
     C_IN => C6_to_C7,
     SUM => SUM(7),
-    C_OUT => C7_to_C8,
+    C_OUT => C7_to_C8
   );
 
   -- Instantiate Full Adder Bit 8
@@ -138,7 +136,7 @@ begin
     B => B(8),
     C_IN => C7_to_C8,
     SUM => SUM(8),
-    C_OUT => C8_to_C9,
+    C_OUT => C8_to_C9
   );
 
   -- Instantiate Full Adder Bit 9
@@ -147,7 +145,7 @@ begin
     B => B(9),
     C_IN => C8_to_C9,
     SUM => SUM(9),
-    C_OUT => C9_to_C10, 
+    C_OUT => C9_to_C10
   );
 
   -- Instantiate Full Adder Bit 10
@@ -156,7 +154,7 @@ begin
     B => B(10),
     C_IN => C9_to_C10,
     SUM => SUM(10),
-    C_OUT => C10_to_C11,
+    C_OUT => C10_to_C11
   );
 
   -- Instantiate Full Adder Bit 11
@@ -165,7 +163,7 @@ begin
     B => B(11),
     C_IN => C10_to_C11,
     SUM => SUM(11),
-    C_OUT => C11_to_C12,
+    C_OUT => C11_to_C12
   );
 
   -- Instantiate Full Adder Bit 12
@@ -174,7 +172,7 @@ begin
     B => B(12),
     C_IN => C11_to_C12,
     SUM => SUM(12),
-    C_OUT => C12_to_C13, 
+    C_OUT => C12_to_C13 
   );
 
   -- Instantiate Full Adder Bit 13
@@ -183,7 +181,7 @@ begin
     B => B(13),
     C_IN => C12_to_C13,
     SUM => SUM(13),
-    C_OUT => C13_to_C14,
+    C_OUT => C13_to_C14
   );
 
   -- Instantiate Full Adder Bit 14
@@ -192,7 +190,7 @@ begin
     B => B(14),
     C_IN => C13_to_C14,
     SUM => SUM(14),
-    C_OUT => C14_to_C15,
+    C_OUT => C14_to_C15
   );
 
   -- Instantiate Full Adder Bit 15
@@ -201,7 +199,7 @@ begin
     B => B(15),
     C_IN => C14_to_C15,
     SUM => SUM(15),
-    C_OUT => C15_to_C16, 
+    C_OUT => C15_to_C16 
   );
 
   -- Instantiate Full Adder Bit 16
@@ -210,7 +208,7 @@ begin
     B => B(16),
     C_IN => C15_to_C16,
     SUM => SUM(16),
-    C_OUT => C16_to_C17,
+    C_OUT => C16_to_C17
   );
 
   -- Instantiate Full Adder Bit 17
@@ -219,7 +217,7 @@ begin
     B => B(17),
     C_IN => C16_to_C17,
     SUM => SUM(17),
-    C_OUT => C17_to_C18,
+    C_OUT => C17_to_C18
   );
 
   -- Instantiate Full Adder Bit 18
@@ -228,7 +226,7 @@ begin
     B => B(18),
     C_IN => C17_to_C18,
     SUM => SUM(18),
-    C_OUT => C18_to_C19,
+    C_OUT => C18_to_C19
   );
 
   -- Instantiate Full Adder Bit 19
@@ -237,7 +235,7 @@ begin
     B => B(19),
     C_IN => C18_to_C19,
     SUM => SUM(19),
-    C_OUT => C19_to_C20,
+    C_OUT => C19_to_C20
   );
 
   -- Instantiate Full Adder Bit 20
@@ -246,7 +244,7 @@ begin
     B => B(20),
     C_IN => C19_to_C20,
     SUM => SUM(20),
-    C_OUT => C20_to_C21,
+    C_OUT => C20_to_C21
   );
 
   -- Instantiate Full Adder Bit 21
@@ -255,7 +253,7 @@ begin
     B => B(21),
     C_IN => C20_to_C21,
     SUM => SUM(21),
-    C_OUT => C21_to_C22,
+    C_OUT => C21_to_C22
   );
 
   -- Instantiate Full Adder Bit 22
@@ -264,7 +262,7 @@ begin
     B => B(22),
     C_IN => C21_to_C22,
     SUM => SUM(22),
-    C_OUT => C22_to_C23,
+    C_OUT => C22_to_C23
   );
 
   -- Instantiate Full Adder Bit 23
@@ -273,7 +271,7 @@ begin
     B => B(23),
     C_IN => C22_to_C23,
     SUM => SUM(23),
-    C_OUT => C23_to_C24,
+    C_OUT => C23_to_C24
   );
 
   -- Instantiate Full Adder Bit 24
@@ -282,7 +280,7 @@ begin
     B => B(24),
     C_IN => C23_to_C24,
     SUM => SUM(24),
-    C_OUT => C24_to_C25,
+    C_OUT => C24_to_C25
   );
 
   -- Instantiate Full Adder Bit 25
@@ -291,7 +289,7 @@ begin
     B => B(25),
     C_IN => C24_to_C25,
     SUM => SUM(25),
-    C_OUT => C25_to_C26,
+    C_OUT => C25_to_C26
   );
 
   -- Instantiate Full Adder Bit 26
@@ -300,7 +298,7 @@ begin
     B => B(26),
     C_IN => C25_to_C26,
     SUM => SUM(26),
-    C_OUT => C26_to_C27,
+    C_OUT => C26_to_C27
   );
 
   -- Instantiate Full Adder Bit 27
@@ -309,7 +307,7 @@ begin
     B => B(27),
     C_IN => C26_to_C27,
     SUM => SUM(27),
-    C_OUT => C27_to_C28,
+    C_OUT => C27_to_C28
   );
 
   -- Instantiate Full Adder Bit 28
@@ -318,7 +316,7 @@ begin
     B => B(28),
     C_IN => C27_to_C28,
     SUM => SUM(28),
-    C_OUT => C28_to_C29,
+    C_OUT => C28_to_C29
   );
 
   -- Instantiate Full Adder Bit 29
@@ -327,7 +325,7 @@ begin
     B => B(29),
     C_IN => C28_to_C29,
     SUM => SUM(29),
-    C_OUT => C29_to_C30,
+    C_OUT => C29_to_C30
   );
 
   -- Instantiate Full Adder Bit 30
@@ -336,7 +334,7 @@ begin
     B => B(30),
     C_IN => C29_to_C30,
     SUM => SUM(30),
-    C_OUT => C30_to_C31,
+    C_OUT => C30_to_C31
   );
 
   -- Instantiate Full Adder Bit 31
@@ -345,7 +343,7 @@ begin
     B => B(31),
     C_IN => C30_to_C31,
     SUM => SUM(31),
-    C_OUT => C31_to_COut,
+    C_OUT => C31_to_COut
   );
   
     -- Carry and Overflow 
